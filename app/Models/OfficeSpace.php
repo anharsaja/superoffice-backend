48666896bb240addd74a0286c267f7132100e44c
anharsaja <anharmuk12@gmail.com>
2024-12-28 17:48:48 +0700

    ✨ feat: migration

--- app/Models/OfficeSpace.php
+++ app/Models/OfficeSpace.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OfficeSpace extends Model
 {
-    //
-}
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'address',
+        'is_open',
+        'is_full_booked',
+        'price',
+        'duration',
+        'about',
+        'slug',
+        'city_id',
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    
+    
+};
+
+
+
